27873bc57b99ddf32b2d927c8f9946ea568d25fd
luoBHHHHH <10415578+luobhhhhh@user.noreply.gitee.com>
2023-12-14 17:32:18 +0800

    feat:根据环境不同展示不同的title

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
+const title = document.querySelector('.title')
+//获取生产环境信息
+const envInfo = import.meta.env
+//设置标题
+title.innerHTML = envInfo.VITE_APP_TITLE
 
 createApp(App).mount('#app')
